1ad9a1d8d70f139390e9b1432322449de8b42101
elena-chu <elenac@insightec.com>
2021-05-19 15:47:05 +0300

    Compare Mode

---
+++ FusStyles/Ws.Fus.ImageViewer.Interfaces/Entities/ImageMode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ws.Fus.ImageViewer.Interfaces.Entities
+{
+    public enum CompareMode
+    {
+        /// <summary>
+        /// Single Image
+        /// </summary>
+        Simple = 0,
+        /// <summary>
+        /// Shows two images alternately.
+        /// </summary>
+        Flicker,
+        /// <summary>
+        /// Shows two images simultaneously, while one image in the left part of the window and another in the right part.
+        /// </summary>
+        Slider,
+        /// <summary>
+        /// Blend between two images.
+        /// </summary>
+        Fusion
+    }    
+}
--- FusStyles/Ws.Fus.ImageViewer.UI.Wpf/Controls/ToolbarMenu/ToolbarMenu.xaml.cs
+++ FusStyles/Ws.Fus.ImageViewer.UI.Wpf/Controls/ToolbarMenu/ToolbarMenu.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ws.Fus.ImageViewer.Interfaces.Entities;
 
 namespace Ws.Fus.ImageViewer.UI.Wpf.Controls.ToolbarMenu
 {
@@ -34,6 +35,39 @@ namespace Ws.Fus.ImageViewer.UI.Wpf.Controls.ToolbarMenu
                 //_logger.Error(ex, "InitializeComponent");
                 //throw;
             }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem mi = sender as MenuItem;
+            if (mi == null)
+                return;
+
+            CompareMode mode = CompareMode.Simple;
+            if(mi.CommandParameter != null)
+            {
+                mode = (CompareMode)Enum.Parse(typeof(CompareMode), mi.CommandParameter.ToString());
+            }
+
+            DataTemplate contentRes;
+            switch (mode)
+            {
+                case CompareMode.Flicker:
+                    contentRes = (DataTemplate)CompareMenu.FindResource("FlickerModeControl");
+                    break;
+                case CompareMode.Slider:
+                    contentRes = (DataTemplate)CompareMenu.FindResource("SliderModeControl");
+                    break;
+                case CompareMode.Fusion:
+                    contentRes = (DataTemplate)CompareMenu.FindResource("FusionModeControl");
+                    break;
+                case CompareMode.Simple:
+                default:
+                    contentRes = (DataTemplate)CompareMenu.FindResource("SliderModeControl");
+                    break;
+            }
+
+            CompareModeControl.ContentTemplate = contentRes;
         }
     }
 }
