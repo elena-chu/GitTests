df1f09629f788a1f34a1f3f96946e1ccfdbc586b
limoragam <limor@limoragam.com>
2021-08-19 20:33:35 +0300

    Icon-Chevron ToggleButton with context menu TODO: ToggleButton with Combo

--- FusStyles/Extensions.Wpf/Behaviors/LeftClickContextMenu.cs
+++ FusStyles/Extensions.Wpf/Behaviors/LeftClickContextMenu.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 
@@ -89,6 +84,7 @@ namespace Ws.Extensions.UI.Wpf.Behaviors
                 }
 
                 fe.ContextMenu.PlacementTarget = fe;
+                fe.ContextMenu.Placement = ContextMenuService.GetPlacement(fe);
                 fe.ContextMenu.IsOpen = true;
 
                 e.Handled = false;
