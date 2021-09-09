2e7b135c2edc3b9ee9f80690ec4f41f081b4ea5a
limoragam <limor@limoragam.com>
2021-09-09 19:41:16 +0300

    Tasks 10a-c: Plus buttons with Ripple and Circular wipe animations  >> Add ControlState attached property for various states  >> Add AnimatedBorder for wipe animation Expand/Collapse chevron buttons with Disabled state of simple line

--- FusStyles/Ws.Fus.Treatment.UI.Wpf/Views/DebugView.xaml.cs
+++ FusStyles/Ws.Fus.Treatment.UI.Wpf/Views/DebugView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Threading.Tasks;
+using System.Windows.Controls;
+using Ws.Extensions.UI.Wpf.Behaviors;
 
 namespace Ws.Fus.Treatment.UI.Wpf.Views
 {
@@ -7,6 +9,31 @@ namespace Ws.Fus.Treatment.UI.Wpf.Views
         public DebugView()
         {
             InitializeComponent();
+        }
+
+        private void ChangeControlState(object sender, System.Windows.RoutedEventArgs e)
+        {
+            switch (ControlExtensions.GetControlState(RippleButton))
+            {
+                case ControlState.Normal:
+                    ControlExtensions.SetControlState(RippleButton, ControlState.CallToAction);
+                    break;
+                case ControlState.CallToAction:
+                    ControlExtensions.SetControlState(RippleButton, ControlState.Busy);
+                    DelayAndReset();
+                    break;
+                case ControlState.Busy:
+                    ControlExtensions.SetControlState(RippleButton, ControlState.Normal);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private async Task DelayAndReset()
+        {
+            await Task.Delay(8000);
+            ControlExtensions.SetControlState(RippleButton, ControlState.Normal);
         }
     }
 }
