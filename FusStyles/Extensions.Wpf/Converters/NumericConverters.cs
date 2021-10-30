f8ac9c8a7be9517a03379b9be5f12cdaaf35d4c1
limoragam <limor@limoragam.com>
2021-10-30 22:41:20 +0300

    TabControl TODO: Hover. Animation on expand/collapse

--- FusStyles/Extensions.Wpf/Converters/NumericConverters.cs
+++ FusStyles/Extensions.Wpf/Converters/NumericConverters.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace Ws.Extensions.UI.Wpf.Converters
@@ -159,6 +160,30 @@ namespace Ws.Extensions.UI.Wpf.Converters
             }
 
             return diff >= 0.0 ? diff : 0.0;
+        }
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Value 0: ItemsPresenter
+    /// Value 1: an item under ItemsPresenter
+    /// Returns Item's index under ItemsPresenter
+    /// </summary>
+    public class ItemToIndexConverter : MultiConverterMarkupExtension<ItemToIndexConverter>
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!ConverterAssists.CheckAllValuesValidity(values))
+                return Binding.DoNothing;
+
+            if (values[0] is ItemsControl itemsControl && values[1] is DependencyObject item)
+                return itemsControl.ItemContainerGenerator.IndexFromContainer(item);
+
+            return Binding.DoNothing;
         }
 
         public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
